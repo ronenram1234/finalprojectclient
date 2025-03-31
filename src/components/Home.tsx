ebeb3b7d4e3957bd70f2290ff2e72421b833dddc
Ronen Ram <ronenram1234@gmail.com>
2025-03-31 09:19:50 +0300

    home top section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,107 +1,95 @@
 import { FunctionComponent } from "react";
+import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
 
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-// interface Props {
-
-// }
-
-// const Home: FunctionComponent<Props> = () => {
 const Home: FunctionComponent = () => {
   return (
-    <>
-      <div
-        id="carouselExampleSlidesOnly"
-        className="carousel slide"
-        data-bs-ride="carousel"
-        data-bs-interval="6000"
-      >
-        <div className="carousel-inner">
-          {/* Slide 1 */}
-          <div className="carousel-item active">
-            <img
-              src="home1.jpg"
-              className="d-block w-100 block-home"
-              alt="Slide 1"
-            />
-            <div className="carousel-caption custom-caption">
-              <h2 className="carousel-text">FULLY TESTED</h2>
-              <h1 className="carousel-title">MEMORY AND CPUS</h1>
-              <p className="carousel-text-small">NEW AND USED</p>
-            </div>
-          </div>
+    <Carousel interval={4000} pause={false}>
+      {/* Slide 1 */}
+      <Carousel.Item className="custom-caption">
+        <img
+          className="d-block w-100 block-home"
+          src="/home1.jpg"
+          alt="Slide 1"
+        />
+        <Carousel.Caption className="custom-caption">
+          <h2 className="carousel-text">FULLY TESTED</h2>
+          <h1 className="carousel-title">MEMORY AND CPUS</h1>
+          <p className="carousel-text-small">NEW AND USED</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-          {/* Slide 2 */}
-          <div className="carousel-item">
-            <img
-              src="home2.jpg"
-              className="d-block w-100 block-home"
-              alt="Slide 2"
-            />
-            <div className="carousel-caption custom-caption">
-              <h2 className="carousel-text">EXCESS INVENTORY?</h2>
-              <h1 className="carousel-title">SELL US EQUIPMENT</h1>
-              <p className="carousel-text-small">WE BUY RAM AND CPUS</p>
-            </div>
-          </div>
+      {/* Slide 2 */}
+      <Carousel.Item className="custom-caption">
+        <img
+          className="d-block w-100 block-home"
+          src="/home2.jpg"
+          alt="Slide 2"
+        />
+        <Carousel.Caption className="custom-caption">
+          <h2 className="carousel-text">EXCESS INVENTORY?</h2>
+          <h1 className="carousel-title">SELL US EQUIPMENT</h1>
+          <p className="carousel-text-small">WE BUY RAM AND CPUS</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-          {/* Slide 3 */}
-          <div className="carousel-item">
-            <img
-              src="home3.jpg"
-              className="d-block w-100 block-home"
-              alt="Slide 3"
-            />
-            <div className="carousel-caption custom-caption">
-              <h2 className="carousel-text">LOOKING TO BUY?</h2>
-              <h1 className="carousel-title">VISIT US ON EBAY!</h1>
-              <p className="carousel-text-small">MOST ITEMS SHIP FREE</p>
-            </div>
-          </div>
-          <div className="carousel-item active">
-            <img
-              src="home4.jpg"
-              className="d-block w-100 block-home"
-              alt="Slide 1"
-            />
-            <div className="carousel-caption custom-caption">
-              <h2 className="carousel-text">FULLY TESTED</h2>
-              <h1 className="carousel-title">MEMORY AND CPUS</h1>
-              <p className="carousel-text-small">NEW AND USED</p>
-            </div>
-          </div>
+      {/* Slide 3 */}
+      <Carousel.Item className="custom-caption">
+        <img
+          className="d-block w-100 block-home"
+          src="/home3.jpg"
+          alt="Slide 3"
+        />
+        <Carousel.Caption className="custom-caption">
+          <h2 className="carousel-text">LOOKING TO BUY?</h2>
+          <h1 className="carousel-title">VISIT US ON EBAY!</h1>
+          <p className="carousel-text-small">MOST ITEMS SHIP FREE</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-          {/* Slide 2 */}
-          <div className="carousel-item">
-            <img
-              src="home5.jpg"
-              className="d-block w-100 block-home"
-              alt="Slide 2"
-            />
-            <div className="carousel-caption custom-caption">
-              <h2 className="carousel-text">EXCESS INVENTORY?</h2>
-              <h1 className="carousel-title">SELL US EQUIPMENT</h1>
-              <p className="carousel-text-small">WE BUY RAM AND CPUS</p>
-            </div>
-          </div>
+      {/* Slide 4 */}
+      <Carousel.Item className="custom-caption">
+        <img
+          className="d-block w-100 block-home"
+          src="/home4.jpg"
+          alt="Slide 4"
+        />
+        <Carousel.Caption className="custom-caption">
+          <h2 className="carousel-text">FULLY TESTED</h2>
+          <h1 className="carousel-title">MEMORY AND CPUS</h1>
+          <p className="carousel-text-small">NEW AND USED</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-          {/* Slide 3 */}
-          <div className="carousel-item">
-            <img
-              src="home6.jpg"
-              className="d-block w-100 block-home"
-              alt="Slide 3"
-            />
-            <div className="carousel-caption custom-caption">
-              <h2 className="carousel-text">LOOKING TO BUY?</h2>
-              <h1 className="carousel-title">VISIT US ON EBAY!</h1>
-              <p className="carousel-text-small">MOST ITEMS SHIP FREE</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+      {/* Slide 5 */}
+      <Carousel.Item className="custom-caption">
+        <img
+          className="d-block w-100 block-home"
+          src="/home5.jpg"
+          alt="Slide 5"
+        />
+        <Carousel.Caption className="custom-caption">
+          <h2 className="carousel-text">EXCESS INVENTORY?</h2>
+          <h1 className="carousel-title">SELL US EQUIPMENT</h1>
+          <p className="carousel-text-small">WE BUY RAM AND CPUS</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      {/* Slide 6 */}
+      <Carousel.Item className="custom-caption">
+        <img
+          className="d-block w-100 block-home"
+          src="/home6.jpg"
+          alt="Slide 6"
+        />
+        <Carousel.Caption className="custom-caption">
+          <h2 className="carousel-text">LOOKING TO BUY?</h2>
+          <h1 className="carousel-title">VISIT US ON EBAY!</h1>
+          <p className="carousel-text-small">MOST ITEMS SHIP FREE</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
